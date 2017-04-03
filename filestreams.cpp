1c7ff3606457daaf910a4e63543a9578618c967c
Pedro Rodrigues <pedroagrodrigues@gmail.com>
2017-04-03 17:41:29 +0100

    Implementação do carregamento dos txt's para variáveis alocadas dinamicamente

--- filestreams.cpp
+++ filestreams.cpp
@@ -1,8 +1,8 @@
 #include "bibliotecas.h";
 #include <fstream>
-void var_load() {
-	string * destinos, line, * temp = new string[500];
-	ifstream file("txt\\destino.txt");
+void preencher_dados(string * data, string caminho) {
+	string  line, * temp = new string[500];
+	fstream file(caminho);
 	int current = 0;
 	if (file.is_open()) {
 		while (getline(file, line)) {
@@ -13,34 +13,33 @@ void var_load() {
 		}
 		file.close();
 
-		destinos = new string[current];
+		data = new string[current];
 		for (int i = 0; i < current; i++) {
-			destinos[i] = temp[i];
+			data[i] = temp[i];
 		}
 		delete[] temp; //Array temporário, já cumpriu a sua função, pode ser apagado para libertar o seu espaço em memória 
 		
-		//verificação de carregamento			   
+		//verificação de carregamento	
+		/*
 		for (int i = 0; i < current; i++) {
-			cout << destinos[i] << endl;
+			cout << data[i] << endl;
 		}
-
+		*/
 
 	} else cout << "ERROR, something prevent connection to the file!\n";
 		
 	
 
 }
-/*
-void preencher_dados(aviao a[], int size) {
-	for (int i = 0; i < size; i++) {
-		a[i].nome_voo = random("voo.txt");
-		a[i].destino = random("destino.txt");
-		a[i].modelo = random("modelo.txt");
-		a[i].origem = random("origem.txt");
-		a[i].capacidade = (rand() % (15 - 5)) + 5;
-		/*a.passageiro.bi=14047585;
-		a.passageiro.nacionalidade=random("nacionalidade.txt");
-		a.passageiro.primeiro_nome=random("primeiro_nome.txt");
-		a.passageiro.segundo_nome=random("segundo_nome.txt");
-		}
-		}*/
+void var_load() {
+	string *destino_file = 0, *capacidade_file = 0, *modelo_file = 0, *nacionalidade_file = 0, *origem_file = 0, *primeiro_file = 0, *segundo_file = 0, *voo_file = 0;
+	preencher_dados(destino_file, "txt\\destino.txt");
+	preencher_dados(capacidade_file, "txt\\capacidade.txt");
+	preencher_dados(modelo_file, "txt\\modelo.txt");
+	preencher_dados(nacionalidade_file, "txt\\nacionalidade.txt");
+	preencher_dados(origem_file, "txt\\origem.txt");
+	preencher_dados(primeiro_file, "txt\\primeiro_nome.txt");
+	preencher_dados(segundo_file, "txt\\segundo_nome.txt");
+	preencher_dados(voo_file, "txt\\voo.txt");
+
+}
